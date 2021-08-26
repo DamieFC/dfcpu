b538bc34329c1e8d05d98d90d5f692c82a91cc16
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-26 08:59:04 -0700

    :)

--- src/dfcpu.c
+++ src/dfcpu.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int dfcpu(char usage[], char args[]) {
+  if(!usage || !args) {
+    perror("No something\n");
+    exit(EXIT_FAILURE);
+  }
+  printf("Hello world!");
+}
