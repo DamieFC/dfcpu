d528d9837de27304459b2bdbdbf4a0d088f1b15a
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-03 16:27:16 -0700

    Added headers and fixed crap that needed fixing

--- src/dfcpu.c
+++ src/dfcpu.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <org_asm.h>
+#include <micro.h>
 
 int dfcpu(char usage[], char args[]) {
   if(!usage || !args) {
     perror("No something. Either no usage or no args.\n");
     exit(EXIT_FAILURE);
   }
-  else if(usage[0] == "hello") {
+  else if(strcmp(usage[0], "hello") == 0) {
     printf("Hello world!");
     return 0;
   }
-  else if(usage[0] == "asm") {
+  else if(strcmp(usage[0] == "asm") == 0) {
     org_asm(args[0]);
   }
 }
